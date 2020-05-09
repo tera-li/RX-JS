b44af302946c304e1ca96a23d3bc4277c8c2faf6
linjinlei <776745393@qq.com>
2020-05-09 18:21:44 +0800

    frist

--- src_ts/index.ts
+++ src_ts/index.ts
@@ -1,2 +1,10 @@
-import {Observable} from 'rxjs';
+import { range } from "rxjs";
+import { map, filter } from "rxjs/operators";
+
+range(1, 200)
+  .pipe(
+    filter(x => x % 2 === 1),
+    map(x => x + x)
+  )
+  .subscribe(x => console.log(x));
 console.log('qweqwe');
