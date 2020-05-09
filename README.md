5a5d5e531d44977bb76225161b85e152daf3e616
linjinlei <776745393@qq.com>
2020-05-09 10:44:26 +0800

    first

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 安装包管理工具
 npm install
-运行
+
+运行项目
 npm run dev
+
 开发环境打包
 npm run build--dev
+
 生产环境打包
 npm run build-prod
+
 自动格式化src_ts下面的所有.ts文件
 npm run style:prettier
+
 运行src_ts下面的index.ts文件
 npm start
+
 运行tslint
 npm run style:lint
 tslint.json的配置：
@@ -17,5 +23,6 @@ tslint.json的配置：
 "linterOptions": {
         "exclude": [ "**/node_modules/**" ]  // 排除 node_modules
     },
+
 使用git commitizen，规范git commit
 commitizen init cz-conventional-changelog --save --save-exact （git cz）
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,34 @@ const htmlwebpackplugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: __dirname + '/src/index.js',  // 入口文件
+    // entry: __dirname + '/src/index.js',  // src/js   入口文件
+    entry: __dirname + '/src_ts/index.ts', // src_ts/ts   入口文件
     output: {
-        path: __dirname + '/dist',  // 出口文件
+        path: __dirname + '/dist', // 出口文件
         filename: 'index.js'
     },
     plugins: [
         new htmlwebpackplugin({
-            template: __dirname + '/src/index.html'   // 运行入口index.html文件
+            // template: __dirname + '/src/index.html' // 运行入口src/index.html文件
+            template: __dirname + '/src_ts/index.html' // 运行入口src_tsindex.html文件
         })
     ],
     module: {
-        rules: [
-            {
-                test: /\.js$/,            // 解析js文件
+        rules: [{
+                test: /\.js$/, // 解析js文件
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
                 }
-            }
+            },
+            {
+                test: /\.tsx?$/, // 解析ts文件
+                use: {
+                    loader: 'ts-loader'
+                }
+
+            },
         ]
     },
-    devtool: 'source-map'   // js文件编译
+    devtool: 'source-map' // js文件编译
 }
