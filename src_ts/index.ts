6564d587357e3a80b172ced2e3f816a7df9e37da
linjinlei <776745393@qq.com>
2020-05-13 09:30:38 +0800

    feat(添加): commit规范

--- src_ts/index.ts
+++ src_ts/index.ts
@@ -216,6 +216,6 @@ import { fromEvent } from 'rxjs';
 // 创建发出点击事件的 observable
 const source = fromEvent(document.getElementsByTagName('h1'), 'click');
 // 映射成给定的事件时间戳
-const example = source.pipe(map(event => `Event time: ${event}`));
+const example = source.pipe(map(event => `Event time: ${event.timeStamp}`));
 // 输出 (示例中的数字以运行时为准): 'Event time: 7276.390000000001'
 const subscribe = example.subscribe(val => console.log(val));
